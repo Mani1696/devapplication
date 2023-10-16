7717cbf7d29fa602b55f65236ced5161d0622a4e
Mani1696 <manibharathi809@gmail.com>
2023-10-16 10:36:32 +0530

    First commit

---
+++ src/main/java/com/example/DevApplication/DevController.java
@@ -0,0 +1,16 @@
+package com.example.DevApplication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DevController {
+	@Autowired
+	@GetMapping(value="/Get")
+	public String get() {
+		return "Hellow World";
+	}
+
+
+}
